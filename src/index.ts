3d6b4e6e0083b90bbafd03e162f499cb3b537a0a
Ivor Lewis <101133399+00ives@users.noreply.github.com>
2023-08-03 14:47:08 -0600

    add functionality for adding items to the list

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,39 @@
-// import { v4 as uuidV4 } from 'uuid';
+import { V4Options, v4 as uuidV4 } from 'uuid';
 
 // console.log(uuidV4());
 
+type Task = {
+  id: any;
+  title: string;
+  completed: boolean;
+  createdAt: Date;
+};
+
 const list = document.querySelector<HTMLUListElement>('#list');
-const form = document.querySelector<HTMLFormElement>('#new-task-form');
+const form = document.getElementById('new-task-form') as HTMLFormElement | null;
 const input = document.querySelector<HTMLInputElement>('#new-task-title');
+
+form?.addEventListener('submit', (e) => {
+  e.preventDefault();
+  if (input?.value == '' || input?.value == null) return;
+
+  const newTask: Task = {
+    id: uuidV4,
+    title: input.value,
+    completed: false,
+    createdAt: new Date(),
+  };
+  input.value;
+
+  addListItem(newTask);
+});
+
+const addListItem = (task: Task) => {
+  const item = document.createElement('li');
+  const label = document.createElement('label');
+  const checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+  label.append(checkbox, task.title);
+  item.append(label);
+  list?.append(item);
+};
